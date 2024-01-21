6d5ac02f1eb038e37e94baf93a916c5610901a8f
VelDLumina2942 <145996670+VelDLumina2942@users.noreply.github.com>
2024-01-21 12:46:41 +0900

    入れ替え実装できませんでした

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -8,8 +8,35 @@
 // begin: ソートする配列の先頭要素へのポインタ
 // end: ソートする配列の末尾要素の次の要素へのポインタ
 // radix: 基数
+
 bool radix_sort(item* begin, const item* end, int radix)
 {
-	// ToDo: 基数ソートを実装する
-	return false;
+    // ToDo: 基数ソートを実装する
+    if (begin == NULL || end == NULL)return false;
+    if (end - begin < 1)return false;
+    if (radix < 1)return false;
+    if (end - begin <= 1)return true;
+    item* p = begin;
+    item* q = end;
+    int rad[10];
+    int i, j, k;
+    int m = 1;
+    item* back = begin + (end - begin) - 1;
+   
+    while (m <= back->key) {
+        for (i = 0; i < back->key; i++)
+            rad[i] = (p[i].key / m) % radix;
+
+        k = 0;
+        for (i = 0; i <= 9; i++)
+            for (j = 0; j < back->key; j++)
+                if (rad[j] == i)
+                    q[k++] = p[j];
+
+        for (i = 0; i < back; i++)
+            p[i] = q[i];
+
+        m *= 10;
+        radix_sort(begin,end,radix);
+    }
 }
