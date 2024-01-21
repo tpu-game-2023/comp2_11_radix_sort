675609f0b1883e59efc0e8d194c8c3ee390467ae
2227004 <g2227004@st.t-kougei.ac.jp>
2024-01-21 19:42:14 +0900

    未完成です

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -11,5 +11,39 @@
 bool radix_sort(item* begin, const item* end, int radix)
 {
 	// ToDo: 基数ソートを実装する
-	return false;
+	if(begin==NULL||end==NULL)return false;
+}
+int getMax(int begin[], int end)
+{
+	int max = begin[0];
+	for (int i = 1; i < end; i++)
+		if (begin[i] > max)
+			max = begin[i];
+	return max;
+}
+void CountingSort(int begin[], int end, int radix)
+{
+	int output[1];
+	int count[10] = { 0 };
+
+	for (int i = 0; i < end; i++)
+		count[(begin[i] / radix) % 10]++;
+
+	for (int i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+
+	for (int i = end - 1; i >= 0; i--)
+	{
+		output[count[(begin[i] / radix) % 10] - 1] = begin[i];
+		count[(begin[i] / radix) % 10]--;
+	}
+
+	for (int i = 0; i < end; i++)
+		begin[i] = output[i];
+}
+void RadixSort(int begin[], int end)
+{
+	int m = getMax(begin, end);
+	for (int radix = 1; m / radix > 0; radix *= 10)
+		CountingSort(begin, end, radix);
 }
